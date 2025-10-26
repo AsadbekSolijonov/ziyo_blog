0150fd0dc6b895b63acbd4d36b60ecd9b764db30
SolijonovAsadbek <109437399+AsadbekSolijonov@users.noreply.github.com>
2025-10-26 14:02:36 +0500

    more changes

--- content/urls.py
+++ content/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from content import views
 
 urlpatterns = [
-    path('', views.blogs),
-    path('<int:pk>/', views.single_blog),
     path('cbv-lc/', views.BlogListCreateView.as_view()),
     path('cbv-rud/<int:pk>', views.BlogRetrieveUpdateDestoryView.as_view()),
     path('my/', views.MyListBlogView.as_view())
