13ad609331a0bc12761700a958df70f3b50dd541
SolijonovAsadbek <109437399+AsadbekSolijonov@users.noreply.github.com>
2025-10-26 13:49:01 +0500

    added cbv

--- content/urls.py
+++ content/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.blogs),
     path('<int:pk>/', views.single_blog),
     path('cbv-lc/', views.BlogListCreateView.as_view()),
-    path('cbv-rud/<int:pk>', views.BlogRetrieveUpdateDestoryView.as_view())
+    path('cbv-rud/<int:pk>', views.BlogRetrieveUpdateDestoryView.as_view()),
+    path('my/', views.MyListBlogView.as_view())
 ]
--- content/views.py
+++ content/views.py
@@ -8,21 +8,6 @@ from content.models import Blog
 from content.serializers import BlogSerializer
 
 
-# Function Based Views (FBV)  #CRUD - Create, Read, Update, Delete
-@api_view(['GET', 'POST'])
-def blogs(request):
-    if request.method == 'GET':  # List
-        blogs_ = Blog.objects.all()
-        serializer = BlogSerializer(blogs_, many=True, context={"request": request})
-        return Response(serializer.data)
-
-    elif request.method == 'POST':  # Create
-        serializer = BlogSerializer(data=request.data, context={"request": request.data})
-        serializer.is_valid(raise_exception=True)
-        serializer.save(user=request.user)
-        return Response(serializer.data)
-
-
 class BlogListCreateView(generics.ListCreateAPIView):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
@@ -31,31 +16,14 @@ class BlogListCreateView(generics.ListCreateAPIView):
         serializer.save(user=self.request.user)
 
 
-@api_view(['GET', 'PATCH', 'DELETE'])  # pk - id
-def single_blog(request, pk):
-    blog = get_object_or_404(Blog, id=pk)
+class MyListBlogView(generics.ListAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
 
-    if request.method == 'GET':
-        serializer = BlogSerializer(blog)
-        return Response(serializer.data)
-
-    # if request.method == 'PUT':
-    #     serializer = BlogSerializer(blog, data=request.data)
-    #     serializer.is_valid(raise_exception=True)
-    #     serializer.save(user=request.user)
-    #     return Response(serializer.data)
-
-    if request.method == 'PATCH':
-        serializer = BlogSerializer(blog, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save(user=request.user)
-        return Response(serializer.data)
-
-    # paritail Update
-
-    if request.method == 'DELETE':
-        blog.delete()
-        return Response(status=204)
+    def get_queryset(self):
+        if not self.request.user.is_superuser:
+            return Blog.objects.filter(user=self.request.user)
+        return Blog.objects.all()
 
 
 class BlogRetrieveUpdateDestoryView(generics.RetrieveUpdateDestroyAPIView):
